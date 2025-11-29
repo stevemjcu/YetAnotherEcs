a457d09d24e6214c8b8403812238832d1eb92fdd
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2025-11-28 23:48:30 -0500

    Add sample project

---
+++ YetAnotherEcs.Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace YetAnotherEcs.Sample;
+
+internal class Program
+{
+	static void Main(string[] _)
+	{
+		var world = new World();
+		world.Register<Position>();
+
+		var entity = world.Create();
+		entity.Set<Position>(new(6, 7));
+	}
+
+	internal record struct Position(int X, int Y) : IComponent<Position>;
+}

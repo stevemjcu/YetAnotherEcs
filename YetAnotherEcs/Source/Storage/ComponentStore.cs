d731e9f53e23e3a880722b33cb9a875ea27c003f
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2025-12-01 19:56:45 -0500

    Begin support for indexed components

--- YetAnotherEcs/Source/Storage/ComponentStore.cs
+++ YetAnotherEcs/Source/Storage/ComponentStore.cs
@@ -14,22 +14,49 @@ internal class ComponentStore
 	// FIXME: Support bitmasks for more than 32 components, if necessary
 	public static int Bitmask<T>() where T : struct => 1 << Id<T>();
 
-	public void Set<T>(int id, T component) where T : struct => GetStore<T>()[id] = component;
+	public void Set<T>(int id, T component) where T : struct => GetStore<T>().Set(id, component);
 
 	public void Remove<T>(int id) where T : struct => GetStore<T>().Remove(id);
 
-	public T Get<T>(int id) where T : struct => GetStore<T>()[id];
+	public T Get<T>(int id) where T : struct => GetStore<T>().Get(id);
 
-	private Dictionary<int, T> GetStore<T>() where T : struct
+	private ComponentStore<T> GetStore<T>() where T : struct
 	{
 		var id = Id<T>();
 
 		if (!StoreByTypeId.TryGetValue(id, out var value))
 		{
-			value = new Dictionary<int, T>();
+			value = new ComponentStore<T>();
 			StoreByTypeId.Add(id, value);
 		}
 
-		return (Dictionary<int, T>)value;
+		return (ComponentStore<T>)value;
 	}
+}
+
+internal class ComponentStore<T> where T : struct
+{
+	bool Indexed = false;
+
+	private readonly Dictionary<int, T> ComponentById = [];
+	private readonly Dictionary<T, HashSet<int>> IdSetByComponent = [];
+
+	public void Set(int id, T component)
+	{
+		if (Indexed)
+		{
+			IdSetByComponent[Get(id)].Remove(id);
+			IdSetByComponent[component].Add(id);
+		}
+
+		ComponentById[id] = component;
+	}
+
+	public void Remove(int id)
+	{
+		if (Indexed) IdSetByComponent[Get(id)].Remove(id);
+		ComponentById.Remove(id);
+	}
+
+	public T Get(int id) => ComponentById[id];
 }

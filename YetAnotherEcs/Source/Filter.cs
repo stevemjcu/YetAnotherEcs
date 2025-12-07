fdf19e09df7a37b100cff02afcc1666092204231
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2025-12-07 17:33:59 -0500

    Add varadic overloads for filters

--- YetAnotherEcs/Source/Filter.cs
+++ YetAnotherEcs/Source/Filter.cs
@@ -13,10 +13,78 @@ public record struct Filter
 		return this;
 	}
 
+	public Filter Include<T, U>()
+		where T : struct
+		where U : struct
+	{
+		return Include<T>().Include<U>();
+	}
+
+	public Filter Include<T, U, V>()
+		where T : struct
+		where U : struct
+		where V : struct
+	{
+		return Include<T, U>().Include<V>();
+	}
+
+	public Filter Include<T, U, V, W>()
+		where T : struct
+		where U : struct
+		where V : struct
+		where W : struct
+	{
+		return Include<T, U, V>().Include<W>();
+	}
+
+	public Filter Include<T, U, V, W, X>()
+		where T : struct
+		where U : struct
+		where V : struct
+		where W : struct
+		where X : struct
+	{
+		return Include<T, U, V, W>().Include<X>();
+	}
+
 	public Filter Exclude<T>() where T : struct
 	{
 		ExcludeBitmask |= Registry.GetBitmask<T>();
 		return this;
+	}
+
+	public Filter Exclude<T, U>()
+		where T : struct
+		where U : struct
+	{
+		return Exclude<T>().Exclude<U>();
+	}
+
+	public Filter Exclude<T, U, V>()
+		where T : struct
+		where U : struct
+		where V : struct
+	{
+		return Exclude<T, U>().Exclude<V>();
+	}
+
+	public Filter Exclude<T, U, V, W>()
+		where T : struct
+		where U : struct
+		where V : struct
+		where W : struct
+	{
+		return Exclude<T, U, V>().Exclude<W>();
+	}
+
+	public Filter Exclude<T, U, V, W, X>()
+		where T : struct
+		where U : struct
+		where V : struct
+		where W : struct
+		where X : struct
+	{
+		return Exclude<T, U, V, W>().Exclude<X>();
 	}
 
 	public readonly bool Compare(int bitmask)
